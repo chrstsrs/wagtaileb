8726e712d7916d78439409b7aa955fbf5ae4f178
Charisios Tsiairis <chrstsrs@yahoo.gr>
2024-05-15 23:47:36 +0100

    init

---
+++ wagtailweb/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ul8xd^2lwk))4vc26&la*b-ga46y**5r*fxjx9g1q^=5s0sqxf"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
